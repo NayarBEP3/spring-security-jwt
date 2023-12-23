7ba604612356a6ad3ebeac324b2644ecdb30dcbe
NayarBEP3 <brayan.estrada.work@gmail.com>
2023-12-23 14:13:35 -0500

    first commit

---
+++ src/test/java/com/tiamat/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiamat.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
